42bd1bca6b30ee202b309dbba1246bd5d145ea20
Julien Klaer <klaer.julien@gmail.com>
2025-03-05 20:08:55 +0100

    feat: first naive wrapping of trivy scanner

--- trivy/main.go
+++ trivy/main.go
@@ -1,7 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"time"
+)
+
+var image string
+
+func init() {
+	const (
+		defaultImage = "docker.io/library/alpine"
+		usage        = "the image to scan"
+	)
+	flag.StringVar(&image, "image", defaultImage, usage)
+	flag.StringVar(&image, "i", defaultImage, usage+" (shorthand)")
+}
+
+type TrivyScanner struct {
+	binaryPath string
+}
+
+func NewTrivyScanner() (*TrivyScanner, error) {
+	path, err := exec.LookPath("trivy")
+	if err != nil {
+		return nil, fmt.Errorf("look path: %w", err)
+	}
+	return &TrivyScanner{
+		binaryPath: path,
+	}, err
+}
+
+func (t *TrivyScanner) Scan(ctx context.Context, image string) error {
+	tCtx, cancel := context.WithTimeout(ctx, 1*time.Minute)
+	defer cancel()
+
+	cmd := exec.CommandContext(tCtx, t.binaryPath, "image", image)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	return cmd.Run()
+}
 
 func main() {
-	fmt.Println("trivy cli wrapper")
+	flag.Parse()
+	fmt.Println(image)
+
+	s, err := NewTrivyScanner()
+	if err != nil {
+		log.Fatalf("new trivy scanner: %s\n", err)
+	}
+
+	err = s.Scan(context.Background(), image)
+	if err != nil {
+		log.Printf("scan: %s\n", err)
+	}
 }
