9e8112811d91bddf613408aea5d00e7f3ae85b5b
Julien Klaer <j.klaer@tadaweb.com>
2025-03-31 18:03:32 +0200

    feat: creating generic command for new scanners

--- scanner/cmd/trivy/main.go
+++ scanner/cmd/trivy/main.go
@@ -2,60 +2,21 @@ package main
 
 import (
 	"context"
-	"flag"
-	"github.com/MadJlzz/madvsa/scanner/internal/pkg/storage"
+	"github.com/MadJlzz/madvsa/scanner/internal/pkg/cmd"
 	"github.com/MadJlzz/madvsa/scanner/internal/trivy"
 	"log"
-	"net/url"
-	"time"
 )
 
-var executionId string
-var image string
-var output string
-
-func init() {
-	const (
-		defaultImage     = "docker.io/library/alpine"
-		imageUsage       = "the image to scan"
-		executionIdUsage = "the reference to use for the scan operation"
-		defaultOutput    = "file:///tmp"
-		outputUsage      = "the path where to store the file at. (e.g. file:///path/to/scan or s3://path/to/bucket, ...)"
-	)
-	defaultExecutionId := time.Now().Format(time.RFC3339)
-	flag.StringVar(&image, "image", defaultImage, imageUsage)
-	flag.StringVar(&image, "i", defaultImage, imageUsage+" (shorthand)")
-	flag.StringVar(&executionId, "id", defaultExecutionId, executionIdUsage)
-	flag.StringVar(&output, "output", defaultOutput, outputUsage+" (shorthand)")
-}
-
 func main() {
-	flag.Parse()
-
 	ctx := context.Background()
-
-	u, err := url.Parse(output)
-	if err != nil {
-		log.Fatalf("failed to parse output: %s\n", err)
-	}
 
 	s, err := trivy.New()
 	if err != nil {
 		log.Fatalf("new trivy scanner: %s\n", err)
 	}
 
-	storerFactory, err := storage.NewStorerFactory(ctx, u)
-	if err != nil {
-		log.Fatalf("failed to init storer factory: %s\n", err)
-	}
-
-	b, err := s.Scan(context.Background(), image)
-	if err != nil {
-		log.Fatalf("failed to scan: %s\n", err)
-	}
-
-	err = storerFactory.Store(ctx, b, u)
-	if err != nil {
-		log.Fatalf("failed to store: %s\n", err)
+	trivyCmd := cmd.NewCommand(s)
+	if err = trivyCmd.Execute(ctx); err != nil {
+		log.Fatalf("trivy scanner: %s\n", err)
 	}
 }
--- scanner/internal/pkg/cmd/command.go
+++ scanner/internal/pkg/cmd/command.go
@@ -1,1 +1,64 @@
 package cmd
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"github.com/MadJlzz/madvsa/scanner/internal/pkg/storage"
+	"github.com/MadJlzz/madvsa/scanner/internal/pkg/vuln"
+	"net/url"
+	"time"
+)
+
+const (
+	defaultImage     = "docker.io/library/alpine"
+	imageUsage       = "the image to scan"
+	executionIdUsage = "the reference to use for the scan operation"
+	defaultOutput    = "file:///tmp"
+	outputUsage      = "the path where to store the file at. (e.g. file:///path/to/scan or s3://path/to/bucket, ...)"
+)
+
+type args struct {
+	executionId *string
+	image       *string
+	output      *string
+}
+
+type Command struct {
+	args    args
+	scanner vuln.Scanner
+}
+
+func NewCommand(scanner vuln.Scanner) *Command {
+	return &Command{scanner: scanner, args: args{
+		executionId: flag.String("id", time.Now().Format(time.RFC3339), executionIdUsage),
+		image:       flag.String("image", defaultImage, imageUsage),
+		output:      flag.String("output", defaultOutput, outputUsage),
+	}}
+}
+
+func (c *Command) Execute(ctx context.Context) error {
+	flag.Parse()
+
+	u, err := url.Parse(*c.args.output)
+	if err != nil {
+		return fmt.Errorf("failed to parse output: %s\n", err)
+	}
+
+	storerFactory, err := storage.NewStorerFactory(ctx, u)
+	if err != nil {
+		return fmt.Errorf("failed to init storer factory: %s\n", err)
+	}
+
+	b, err := c.scanner.Scan(context.Background(), *c.args.image)
+	if err != nil {
+		return fmt.Errorf("failed to scan: %s\n", err)
+	}
+
+	err = storerFactory.Store(ctx, b, u)
+	if err != nil {
+		return fmt.Errorf("failed to store: %s\n", err)
+	}
+
+	return nil
+}
