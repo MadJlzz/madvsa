7110c3ba0f1a0a97ca62bbdcda80917221c1f720
Julien Klaer <klaer.julien@gmail.com>
2025-04-19 17:15:42 +0200

    feat: hardcoded scanning inside Kubernetes using trivy wrapper

--- README.md
+++ README.md
@@ -3,33 +3,52 @@
 `madvsa` is a vulnerability scanners orchestrator that aims to scan OCI artifacts in multiple ways. It can be periodic,
 just in time, or even triggered based on supported events.
 
+# Roadmap
+
+- Find a way not to have to rebuild everytime docker images whenever I have to change something in the code.
+
+- Create and use one abstraction layer to create scans between Docker and Kubernetes to avoid rewriting all
+  the business logic over and over again
+
+- Make sure we can let the user decide to mount a volume inside the pod for scans results storage
+- Configure the pod we start to be able to send data over S3 like storage. (GCS implemented, need config of the pod)
+
+- Handle cronjobs to periodically scan images
+    - predefined in a list
+    - fetched from an OCI registry
+
 # Developing
 
 I use Golang to develop all components, whether it's the controlplane or the wrapper used to run the scanners.
 
 To get started, start the controlplane
+
 ```go
 cd controlplane && go run *.go
 ```
 
 Build the scanner's image
+
 ```shell
 docker build -t madvsa/grype:latest -f scanner/Dockerfile.grype scanner
 docker build -t madvsa/trivy:latest -f scanner/Dockerfile.trivy scanner
 ```
 
 Trigger scans using the REST API
+
 ```shell
 # This will start a new image scan of alpine:3.17 using Trivy and Docker/Podman as orchestrator of work.
 curl -XPOST 'http://localhost:3000/api/v1/scanner/trivy/trigger?img=alpine:3.17'
 ```
 
 If you prefer to use Kubernetes like I do, you can always spin up a new cluster with `kind`.
+
 ```shell
 kind create cluster --name madvsa
 ```
 
 Now deploy everything inside that local cluster
+
 ```shell
 kubectl apply -f deploy/manifests
 ```
--- controlplane/kubernetes.go
+++ controlplane/kubernetes.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/google/uuid"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
@@ -31,24 +32,37 @@ func NewKubernetesService() *KubernetesService {
 	}
 }
 
-func (k *KubernetesService) Scan(ctx context.Context, scanner string, image string) error {
-	// Define pod
-	pod := &corev1.Pod{
+func generatePodConfiguration(requestId string, scanner string, image string) *corev1.Pod {
+	return &corev1.Pod{
 		ObjectMeta: metav1.ObjectMeta{
-			Name: "custom-command-pod",
+			Name: requestId,
+			//Labels: map[string]string{
+			//	"scanner":    scanner,
+			//	"created-by": "madvsa/controlplane",
+			//},
 		},
 		Spec: corev1.PodSpec{
 			Containers: []corev1.Container{
 				{
-					Name:    "main",
-					Image:   "busybox",
-					Command: []string{"/bin/sh", "-c", "echo 'Hello World!'"},
+					Name:  "main",
+					Image: "madjlzz/madvsa-trivy:latest",
+					Args:  []string{"-image", image, "-output", fmt.Sprintf("file:///%s.grype.txt", requestId)},
 				},
 			},
 			RestartPolicy: corev1.RestartPolicyNever,
 		},
 	}
-	// Create the pod in the default namespace
+}
+
+func (k *KubernetesService) Scan(ctx context.Context, scanner string, image string) error {
+	id, err := uuid.NewV7()
+	if err != nil {
+		return fmt.Errorf("cannot create uuid for pod: %w", err)
+	}
+
+	pod := generatePodConfiguration(id.String(), scanner, image)
+	// TODO: let's see how we could give a user friendly value for that namespace.
+	// 	Maybe config?
 	createdPod, err := k.cli.CoreV1().Pods("default").Create(ctx, pod, metav1.CreateOptions{})
 	if err != nil {
 		panic(fmt.Sprintf("failed to create pod: %v", err))
